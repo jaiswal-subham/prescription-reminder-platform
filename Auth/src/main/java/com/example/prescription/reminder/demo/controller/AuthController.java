d6a23dc6fb93182b872c223ce34ccd52d5b3ed50
Subham Jaiswal <subhamjaiswalamell@gmail.com>
2025-07-06 22:29:48 -0700

    Login done and Microservice project

--- Auth/src/main/java/com/example/prescription/reminder/demo/controller/AuthController.java
+++ Auth/src/main/java/com/example/prescription/reminder/demo/controller/AuthController.java
@@ -24,7 +24,6 @@ public class AuthController {
 
     @PostMapping(ConstantsValues.loginRoute)
     ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest){
-        System.out.println("HI");
         String login = this.userService.login(loginRequest);
         if(login.equals(ConstantsValues.loginFailed)) return new ResponseEntity<LoginResponse>(new LoginResponse(login), HttpStatus.EXPECTATION_FAILED);
         return new ResponseEntity<LoginResponse>(new LoginResponse(login), HttpStatus.OK);
--- Auth/src/main/java/com/example/prescription/reminder/demo/dao/LoginRequest.java
+++ Auth/src/main/java/com/example/prescription/reminder/demo/dao/LoginRequest.java
@@ -2,7 +2,6 @@ package com.example.prescription.reminder.demo.dao;
 
 import com.example.prescription.reminder.demo.utilities.ConstantsValues;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
